785f5cbc04e8730d9083746f8ef362ad2a5d7471
Gustas Laurinavičius <gustas.laurinavicius@stud.techin.lt>
2026-04-01 00:51:22 +0300

    Fix image

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,13 +1,16 @@
 import "./Contacts.css";
+import facebook from "../assets/facebook.png"
+import telephone from "../assets/telephone.png"
+import gmail from "../assets/gmail.png"
 
 function Contacts() {
   return (
     <div className="contacts">
         <h1>Kontaktai</h1>
         <ul className="contacts-ul">
-            <li><img className="contacts-img" src="src/assets/facebook.png"/> Gustas Laurinavičius</li>
-            <li><img className="contacts-img" src="src/assets/telephone.png"/> +37069028313</li>
-            <li><img className="contacts-img" src="src/assets/gmail.png"/> glaurazas@gmail.com</li>
+            <li><img className="contacts-img" src={facebook}/> Gustas Laurinavičius</li>
+            <li><img className="contacts-img" src={telephone}/> +37069028313</li>
+            <li><img className="contacts-img" src={gmail}/> glaurazas@gmail.com</li>
         </ul>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Home.css";
+import foto from '../assets/foto.png';
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,7 +21,7 @@ function Home() {
           <h3>Apie mane</h3>
           <p>Mano vardas yra Gustas.</p> 
           <p>Esu mokinys, man yra 16 metų, mokinuosi 10-oje klasėje, gyvenu Lietuvoje.</p>
-          <img src="src/assets/foto.png" />
+          <img src={foto} />
         </div>
       )}
     </div>
